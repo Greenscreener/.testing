9a16504a631d1e3819f7baadf0c7807ea0f640b6
Greenscreener <honzikcernoh@gmail.com>
2017-02-14 15:42:08 +0100

    Did some stuff, don't remember what.

--- sql/index.php
+++ sql/index.php
@@ -2,13 +2,13 @@
      <head>
          <meta charset="UTF-8" >
          <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
-         <link rel="stylesheet" type="text/css" href="styles/style1.css">
+         <link rel="stylesheet" type="text/css" href="/styles/style1.css">
          </head>
          <body>
 
-              <h3>Hello! Welcome to testing.greenscreener.tk!&nbsp;&nbsp;&nbsp;&nbsp;</h3>
-              This page is made for testing various css/php/js features. Mostly for our later use.
-              These are all the pages that are contained within this subdomain:<br />
+              <h3>Hello! Welcome to testing.greenscreener.tk/sql/!&nbsp;&nbsp;&nbsp;&nbsp;</h3>
+              This page is made for testing various sql features. Mostly for our later use.
+              These are all the pages that are contained within this folder:<br />
 
               <?php
                 $adresar=opendir("./");
@@ -25,7 +25,7 @@
               ?>
 
               <h4>Have a nice day</h4>
-              <a href="//github.com/Greenscreener/.testing"> <img src="github.png" style="text-decoration: none; box-shadow: 0px 0px 5px black" height="50px"> </a>
+              <a href="//github.com/Greenscreener/.testing"> <img src="/github.png" style="text-decoration: none; box-shadow: 0px 0px 5px black" height="50px"> </a>
 
               <div id="endora"><endora></div>
                </body>
---
+++ sql/test1.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$servername = "localhost";
+$username = "ajmen";
+$password = "0212Endora";
+$dbname = "ajmen1448026383";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "INSERT INTO test1 (ID, HLASKA, LIKY, DATUM, PROFESOR) values (0, 'To je tvůj boj bobánku', 0, '2002-02-02', 'Pan zuzovy')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
